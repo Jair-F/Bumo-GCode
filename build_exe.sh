df87fb72fcdd620eaddb603d09aa031bfd36b619
Jair <jair.fehlauer@gmail.com>
2026-02-27 14:41:51 +0000

    fix build

--- build_exe.sh
+++ build_exe.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-mkdir -p dist
-
 wine python -m nuitka --standalone --onefile --jobs=$(nproc) \
     --assume-yes-for-downloads \
     --windows-icon-from-ico=icon/icon.ico \
     --windows-console-mode=disable \
     --mingw64 --lto=no --remove-output \
     --onefile-windows-splash-screen-image=data/splash.png \
-    src/app.py -o dist/app.exe
+    src/app.py -o app.exe
 
 # --onefile-no-compression
 # --onefile-windows-splash-screen-image=data/splash.png
@@ -21,5 +19,5 @@ python3 -m nuitka --standalone --onefile --jobs=$(nproc) \
     --windows-icon-from-ico=icon/icon.ico \
     --windows-console-mode=disable \
     --mingw64 --lto=no --remove-output \
-    src/app.py -o dist/app.exe
+    src/app.py -o app.exe
 COMMENT
